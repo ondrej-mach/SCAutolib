0e6b47117ac2605c70860b1a9f42828a7acc60aa
Ondrej Mach <omach@redhat.com>
2023-01-31 11:27:54 +0100

    Small refactoring.

--- SCAutolib/models/gui.py
+++ SCAutolib/models/gui.py
@@ -2,7 +2,6 @@ from SCAutolib import run
 
 import os
 from time import sleep, time
-import uinput
 import cv2
 import numpy as np
 import pandas as pd
@@ -14,24 +13,23 @@ import functools
 
 
 class Screen():
-    '''Captures the screenshots'''
+    """Captures the screenshots"""
 
-    def __init__(self, directory):
-        self.directory=directory
+    def __init__(self, directory: str):
+        self.directory = directory
         self.screenshot_num = 1
 
     def screenshot(self):
-        '''
+        """
         Runs ffmpeg to take a screenshot.
 
         The filename of screenshot is then returned.
-        '''
+        """
 
         filename = f'/tmp/SC-tests/{self.screenshot_num}.png'
-        out = run(['ffmpeg', '-hide_banner', '-y', '-f', 'kmsgrab', '-i', '-',
-             '-vf', 'hwdownload,format=bgr0', '-frames', '1', '-update', '1',
-             filename
-        ])
+        run(['ffmpeg', '-hide_banner', '-y', '-f', 'kmsgrab', '-i', '-', '-vf',
+             'hwdownload,format=bgr0', '-frames', '1', '-update', '1',
+             filename])
 
         self.screenshot_num += 1
         return filename
@@ -50,31 +48,30 @@ class Mouse():
             uinput.REL_WHEEL,
         ])
 
-    def move(self, x, y):
-        '''Moves the mouse cursor to specified absolute coordinate.'''
+        self.MAX_RES = 10000
+        self.FLICK_TIME = 0.1
+        self.STEP_TIME = 0.005
 
-        # Go to upper left corner
-        self.device.emit(uinput.REL_X, -10000)
-        sleep(0.1)
-        self.device.emit(uinput.REL_Y, -10000)
-        sleep(0.1)
+    def move(self, x: int, y: int):
+        """Moves the mouse cursor to specified absolute coordinate."""
 
-        # Move to correct X coordinate
-        for i in range(x):
-            self.device.emit(uinput.REL_X, 1)
-            sleep(0.005)
+        for uinput_axis, value in [(uinput.REL_X, x), (uinput.REL_Y, y)]:
+            # Go all the way up/left with the mouse
+            sleep(self.FLICK_TIME)
+            self.device.emit(uinput_axis, -self.MAX_RES)
+            sleep(self.FLICK_TIME)
 
-        # Move to correct Y coordinate
-        for i in range(y):
-            self.device.emit(uinput.REL_Y, 1)
-            sleep(0.005)
+            # Go to the exact coordinate
+            for i in range(value):
+                self.device.emit(uinput_axis, 1)
+                sleep(self.STEP_TIME)
 
-    def click(self, button='left'):
-        '''
+    def click(self, button: str = 'left'):
+        """
         Clicks the any button of the mouse.
 
         Button value can be 'left', 'right' or 'middle'.
-        '''
+        """
 
         button_map = {
             'left': uinput.BTN_LEFT,
@@ -90,11 +87,13 @@ class Mouse():
         # release the button
         self.device.emit(uinput_button, 0)
 
-# Wrapper class for keyboard library
+
 class KB():
+    """Wrapper class for keyboard library."""
+
     def __init__(self):
         def kb_decorator(fn):
-            def wrapper(*args,**kwargs):
+            def wrapper(*args, **kwargs):
                 print('keyboard...')
                 fn(*args, **kwargs)
                 sleep(1)
@@ -120,7 +119,6 @@ class GUI():
 
         self.kb = KB()
 
-
     def __enter__(self):
         self.screen = Screen(self.screenshot_directory)
         # By restarting gdm, the system gets into defined state
@@ -131,27 +129,29 @@ class GUI():
 
         return self
 
-
     def __exit__(self, type, value, traceback):
         # Gather the screenshots, generate report
         pass
 
     @staticmethod
     def _image_to_data(filename):
-        '''
+        """
         Convert screenshot into dataframe of words with their coordinates.
-        '''
+        """
         image = cv2.imread(filename)
         grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        upscaled = cv2.resize(grayscale, dsize=None, fx=2, fy=2, interpolation=cv2.INTER_LANCZOS4)
-        thresh, binary = cv2.threshold(grayscale, 120, 255, cv2.THRESH_BINARY_INV)
+        upscaled = cv2.resize(grayscale,
+                              dsize=None, fx=2, fy=2,
+                              interpolation=cv2.INTER_LANCZOS4)
+        _, binary = cv2.threshold(upscaled, 120, 255, cv2.THRESH_BINARY_INV)
         image_data_str = pytesseract.image_to_data(binary)
-        df = pd.read_csv(StringIO(image_data_str), sep='\t', lineterminator='\n')
+        df = pd.read_csv(StringIO(image_data_str),
+                         sep='\t', lineterminator='\n')
         return df
 
     @staticmethod
-    def _images_same(filename1, filename2):
-        '''Compare two images, return True if they are completely identical.'''
+    def _images_same(filename1: str, filename2: str):
+        """Compare two images, return True if they are completely identical."""
         im1 = cv2.imread(filename1)
         im2 = cv2.imread(filename2)
 
@@ -165,8 +165,7 @@ class GUI():
 
         return True
 
-
-    def click_on(self, key: str, timeout=30):
+    def click_on(self, key: str, timeout: float = 30):
         end_time = time() + timeout
 
         # Repeat screenshotting, until the key is found
@@ -188,7 +187,8 @@ class GUI():
             # More than one word matches, choose the first match
             # Probably deterministic, but it should not be relied upon
             else:
-                print('Too many to choose from') # TODO replace the print function
+                print('Too many to choose from')
+                # TODO replace the print function
                 item = df.iloc[0]
 
         if time() >= end_time:
@@ -201,13 +201,14 @@ class GUI():
         self.mouse.click()
         sleep(1)
 
-    def assert_text(self, key: str, timeout=0):
-        '''
+    def assert_text(self, key: str, timeout: float = 0):
+        """
         Given key must be found in a screenshot before the timeout.
 
         If the key is not found, exception is raised.
-        Zero timeout means that only one screenshot will be taken and evaluated.
-        '''
+        Zero timeout means that only one screenshot
+        will be taken and evaluated.
+        """
 
         end_time = time() + timeout
         first = True
@@ -225,13 +226,14 @@ class GUI():
 
         raise Exception('The key was not found.')
 
+    def assert_no_text(self, key: str, timeout: float = 0):
+        """
+        If the given key is found in any screenshot before the timeout,
+        an exception is raised.
 
-    def assert_no_text(self, key: str, timeout=0):
-        '''
-        If the given key is found in any screenshot before the timeout, exception is raised.
-
-        Zero timeout means that only one screenshot will be taken and evaluated.
-        '''
+        Zero timeout means that only one screenshot
+        will be taken and evaluated.
+        """
 
         end_time = time() + timeout
         first = True
@@ -247,15 +249,14 @@ class GUI():
             if selection.sum() != 0:
                 raise Exception('The key was found in the screenshot.')
 
-
-    def wait_still_screen(self, time_still=5, timeout=30):
-        '''
+    def wait_still_screen(self, time_still: float = 5, timeout: float = 30):
+        """
         Wait until the screen content stops changing.
 
         When nothing on screen has changed for time_still seconds, continue.
         If the screen content is changing permanently,
         fail with exception after timeout seconds.
-        '''
+        """
 
         timeout_end = time() + timeout
         time_still_end = time() + time_still
@@ -268,8 +269,9 @@ class GUI():
                 # If capturing fails, try again
                 screenshot = self.screen.screenshot()
 
+            different = not self._images_same(screenshot, last_screenshot)
             # If this is the first loop, there is no last screenshot
-            if last_screenshot is None or not self._images_same(screenshot, last_screenshot):
+            if last_screenshot is None or different:
                 last_screenshot = screenshot
                 # Image has changed, refresh time_still_end
                 time_still_end = time() + time_still
@@ -277,20 +279,3 @@ class GUI():
         # If the loop was ended by timeout, it is an error
         if time() >= timeout_end:
             raise Exception('Screen contents were changing until timeout.')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
